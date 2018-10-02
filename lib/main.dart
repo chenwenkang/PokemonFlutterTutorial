379fcdaeb1b6e2363143235e6d0eb65f5ddf79bd
chenwenkang <cwk1998@hotmail.com>
2018-10-03 04:07:57 +0800

    finished product

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:pokemon_app/pokedetail.dart';
+import 'dart:convert';
+import 'package:pokemon_app/pokemon.dart';
 
-void main() => runApp(MaterialApp(
-      title: "Pokemon App",
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
-    ));
+void main() => SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]).then((_) {
+      runApp(MaterialApp(
+        title: "Pokemon App",
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+      ));
+    });
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,18 +24,22 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-
-var url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+  var url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+  PokeHub pokeHub;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     fetchData();
   }
 
   fetchData() async {
-    var res = http.get(url);
-    print(res);
+    var res = await http.get(url);
+    var json = jsonDecode(res.body);
+    pokeHub = PokeHub.fromJson(json);
+    print(pokeHub.toJson());
+    setState(() {});
   }
 
   @override
@@ -36,9 +49,43 @@ var url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/poke
         title: Text("Pokemon App"),
         backgroundColor: Colors.cyan,
       ),
-      body: Center(
-        child: Text("Hello from Pokemon App"),
-      ),
+      body: pokeHub == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              children: pokeHub.pokemon
+                  .map((pokemon) => Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      PokeDetail(pokemon: pokemon)));
+                        },
+                        child: Card(
+                          elevation: 3.0,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Container(
+                                  height: 100.0,
+                                  width: 100.0,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: NetworkImage(pokemon.img)))),
+                              Text(pokemon.name,
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ),
+                      )))
+                  .toList()),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
